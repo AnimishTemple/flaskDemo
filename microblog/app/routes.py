274fe9c40298bb64b11a4f8b5968533621663745
AnimishTemple <animishtenneti@gmail.com>
2024-05-17 11:30:00 -0400

    added login required decorator and the redirect to the next page if a user is not logged in

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,15 +1,18 @@
 from app import app
 from app.forms import LoginForm
 
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app import db
 from app.models import User
 
+from urllib.parse import urlsplit
+
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -43,7 +46,11 @@ def login():
             return redirect(url_for('login'))
         
         login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
 
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        
         return redirect(url_for('index'))
 
     return render_template('login.html', title='Sign In', form=form)
