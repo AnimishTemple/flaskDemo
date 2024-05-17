a6e5ef7f3600796677481ae25293d0664fd8d735
AnimishTemple <animishtenneti@gmail.com>
2024-05-17 09:45:49 -0400

    Flask-login user loader function

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -8,6 +8,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from flask_login import UserMixin
 
+from app import login
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
 class User(UserMixin, db.Model):
 
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
